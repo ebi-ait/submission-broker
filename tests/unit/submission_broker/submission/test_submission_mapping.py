16930ab24ff9fe0d789900c89a6ef4386408e6ce
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-05-07 11:35:17 +0100

    unittest fixup

--- tests/unit/submission_broker/submission/test_submission_mapping.py
+++ tests/unit/submission_broker/submission/test_submission_mapping.py
@@ -12,69 +12,51 @@ class TestSubmissionMapping(unittest.TestCase):
             HandleCollision.IGNORE
         ]
 
-    def test_mapping_identical_index_update_should_return_same_entity(self):
-        submission = Submission(HandleCollision.UPDATE)
-        entity_type = "test_case"
-        index = "index1"
-        entity1 = submission.map(entity_type, index, {})
-        entity2 = submission.map(entity_type, index, {})
-        self.assertEqual(entity1, entity2)
-
-    def test_mapping_identical_index_overwrite_should_return_same_entity(self):
-        submission = Submission(HandleCollision.OVERWRITE)
-        entity_type = "test_case"
-        index = "index1"
-        entity1 = submission.map(entity_type, index, {})
-        entity2 = submission.map(entity_type, index, {})
-        self.assertEqual(entity1, entity2)
-
-    def test_mapping_identical_index_should_update_entity_attributes(self):
-        submission = Submission(HandleCollision.UPDATE)
-        entity_type = "test_case"
-        index = "index1"
-        attributes1 = {
-            'first_entity': 'old',
-            'both_entities': 'old'
-        }
-        attributes2 = {
-            'second_entity': 'new',
-            'both_entities': 'new'
-        }
-        expected_attributes = {
-            'first_entity': 'old',
-            'second_entity': 'new',
-            'both_entities': 'new'
-        }
-        entity1 = submission.map(entity_type, index, attributes1)
-        entity2 = submission.map(entity_type, index, attributes2)
-        self.assertDictEqual(expected_attributes, entity1.attributes)
-        self.assertDictEqual(expected_attributes, entity2.attributes)
-
-    def test_mapping_identical_index_should_overwrite_entity_attributes(self):
-        submission = Submission(HandleCollision.OVERWRITE)
-        entity_type = "test_case"
-        index = "index1"
-        expected_attributes = {
-            'second_entity': 'new'
-        }
-        entity1 = submission.map(entity_type, index, {'first_entity': 'old'})
-        entity2 = submission.map(entity_type, index, {'second_entity': 'new'})
-        self.assertDictEqual(expected_attributes, entity1.attributes)
-        self.assertDictEqual(expected_attributes, entity2.attributes)
+    def test_mapping_identical_index_should_return_same_entity(self):
+        for collision in self.collision_type:
+            with self.subTest(collision=collision):
+                submission = Submission(collision)
+                entity1 = submission.map(self.entity_type, self.index, {})
+                entity2 = submission.map(self.entity_type, self.index, {})
+                self.assertEqual(entity1, entity2)
 
     def test_mapping_identical_index_should_error(self):
         submission = Submission(HandleCollision.ERROR)
-        entity_type = "test_case"
-        index = "index1"
-        submission.map(entity_type, index, {})
+        submission.map(self.entity_type, self.index, {})
         with self.assertRaises(IndexError):
-            submission.map(entity_type, index, {})
+            submission.map(self.entity_type, self.index, {})
 
     def test_mapping_identical_index_should_ignore_entity_attributes(self):
         submission = Submission(HandleCollision.IGNORE)
-        attributes1 = {'first_entity': 'kept'}
-        attributes2 = {'second_entity': 'ignored'}
-        expected_attributes = {'first_entity': 'kept'}
+        attributes1 = {'first_key': 'kept'}
+        attributes2 = {'second_key': 'ignored'}
+        entity = submission.map(self.entity_type, self.index, attributes1)
+        submission.map(self.entity_type, self.index, attributes2)
+        self.assertEqual(attributes1, entity.attributes)
+
+    def test_mapping_identical_index_should_overwrite_entity_attributes(self):
+        submission = Submission(HandleCollision.OVERWRITE)
+        attributes1 = {'first_key': 'forgotten'}
+        attributes2 = {'second_key': 'overwritten'}
+        entity = submission.map(self.entity_type, self.index, attributes1)
+        submission.map(self.entity_type, self.index, attributes2)
+        self.assertEqual(attributes2, entity.attributes)
+
+    def test_mapping_identical_index_should_update_entity_attributes(self):
+        submission = Submission(HandleCollision.UPDATE)
+        attributes1 = {
+            'initial': 'first',
+            'both': 'first'
+        }
+        attributes2 = {
+            'second': 'new',
+            'both': 'new'
+        }
+        expected_attributes = {
+            'initial': 'first',
+            'second': 'new',
+            'both': 'new'
+        }
         entity = submission.map(self.entity_type, self.index, attributes1)
         submission.map(self.entity_type, self.index, attributes2)
         self.assertDictEqual(expected_attributes, entity.attributes)
@@ -85,5 +67,5 @@ class TestSubmissionMapping(unittest.TestCase):
 
     def test_has_data_should_become_true(self):
         submission = Submission()
-        submission.map('type', 'index', {})
+        submission.map(self.entity_type, self.index, {})
         self.assertTrue(submission.has_data())
